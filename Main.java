feb9b88574781c8d5f535869afcb733ac408c7cd
antonqwerty <Fynjy199388>
2020-02-06 20:55:45 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] list = new int[10];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = rand.nextInt();
+        }
+        Arrays.sort(list);
+        System.out.println(Arrays.toString(list));
+    }
+}
